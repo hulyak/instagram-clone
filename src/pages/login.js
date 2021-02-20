87bac68499a99755fbe7437211756a191be9d6df
hulya <hulyakarakayaa06@gmail.com>
2021-02-19 19:20:35 -0800

    handling login state and validation

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
 import instagramPage from '../images/iphone-with-profile.jpg';
 import logo from '../images/logo.png';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || email === '';
+
+  useEffect(() => {
+    document.title = 'Login - Instagram';
+  }, []);
   return (
     <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
       <div className='flex w-3/5'>
@@ -22,17 +30,25 @@ export default function Login() {
               className='text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2'
               type='text'
               placeholder='Email address'
+              onChange={({ target }) => setEmail(target.value)}
+              value={email}
             />
             <input
               aria-label='Enter your password'
               className='text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2'
               type='password'
               placeholder='Password'
+              onChange={({ target }) => setPassword(target.value)}
+              value={password}
             />
             <button
+              disabled={isInvalid}
               type='submit'
-              className={`bg-blue-500 text-white w-full rounded h-8 font-bold`}
+              className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${
+                isInvalid && 'opacity-50 cursor-not-allowed'
+              }`}
             >
+              {' '}
               Log In
             </button>
           </form>

1771ccff917fc0511589fef197a84f8548f8b55b
hulya <hulyakarakayaa06@gmail.com>
2021-02-23 18:22:25 -0800

    add icons for header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+import UserContext from './context/user';
+
 const Dashboard = lazy(() => import('./pages/dashboard'));
 const Login = lazy(() => import('./pages/login'));
 const SignUp = lazy(() => import('./pages/signup'));
@@ -9,17 +11,19 @@ const NotFound = lazy(() => import('./pages/not-found'));
 
 function App() {
   return (
-    <Router>
-      <Suspense fallback={<p>Loading</p>}>
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.SIGNUP} component={SignUp} />
-          <Route path={ROUTES.PROFILE} component={Profile} />
-          <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
-          <Route component={NotFound} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading</p>}>
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} />
+            <Route path={ROUTES.SIGNUP} component={SignUp} />
+            <Route path={ROUTES.PROFILE} component={Profile} />
+            <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
 

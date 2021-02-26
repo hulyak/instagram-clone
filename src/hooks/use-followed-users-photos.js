0497f26b6c3ece93dc87841e8bda314192be507b
hulya <hulyakarakayaa06@gmail.com>
2021-02-26 10:18:08 -0800

    create used followed user hook

--- src/hooks/use-followed-users-photos.js
+++ src/hooks/use-followed-users-photos.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useContext } from 'react';
 import UserContext from '../context/user';
+import { getUserByUserId } from '../services/firebase';
 
 const useFollowedUsersPhotos = () => {
   const [photos, setPhotos] = useState(null);
@@ -8,7 +9,21 @@ const useFollowedUsersPhotos = () => {
   } = useContext(UserContext);
 
   useEffect(() => {
-    const getTimelinePhotos = async () => {};
+    const getTimelinePhotos = async () => {
+      const followingUserIds = await getUserByUserId(userId);
+
+      if (followingUserIds && followingUserIds[0].following.length > 0) {
+        let followedUserPhotos = await getFollowedPhotos(
+          userId,
+          followingUserIds[0].following
+        );
+
+        // we need to call a function that will get us the photos
+
+        followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated); // newest photos first
+        setPhotos(followedUserPhotos);
+      }
+    };
     getTimelinePhotos();
   }, [userId]);
 
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -24,3 +24,17 @@ export async function getUserByUserId(userId) {
   }));
   return user;
 }
+
+export const getUserFollowedPhotos = async (userId, followingUserIds) => {
+  const result = await firebase
+    .firestore()
+    .collection('photos')
+    .where('userId', 'in', followingUserIds)
+    .get();
+
+  const userFollowedPhotos = result.docs.map((item) => ({
+    ...item.data(),
+    docId: item.id,
+  }));
+  console.log(userFollowedPhotos);
+};
